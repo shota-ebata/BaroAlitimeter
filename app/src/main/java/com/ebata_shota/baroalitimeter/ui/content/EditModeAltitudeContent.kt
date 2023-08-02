7279c9d959618da9acf103bc50f7cd0b9564eb11
ebata-shota <maamaaomosiroi@gmail.com>
2023-08-03 02:50:03 +0900

    Surfaceがなかったので配置して、ダークモードのPreviewを追加

--- app/src/main/java/com/ebata_shota/baroalitimeter/ui/content/EditModeAltitudeContent.kt
+++ app/src/main/java/com/ebata_shota/baroalitimeter/ui/content/EditModeAltitudeContent.kt
@@ -1,9 +1,11 @@
 package com.ebata_shota.baroalitimeter.ui.content
 
+import android.content.res.Configuration
 import android.hardware.SensorManager
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -25,48 +27,59 @@ fun EditModeAltitudeContent(
     onClickCancel: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Column(
-        modifier = modifier.padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+    Surface(
+        modifier = modifier
     ) {
-        Row(
-            modifier = modifier.padding(8.dp)
+        Column(
+            modifier = modifier.padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                modifier = modifier,
-                fontSize = 40.sp,
-                text = "$pressureText hPa",
+            Row(
+                modifier = modifier.padding(8.dp)
+            ) {
+                Text(
+                    modifier = modifier,
+                    fontSize = 40.sp,
+                    text = "$pressureText hPa",
+                )
+            }
+
+            Row(
+                modifier = modifier.padding(
+                    top = 8.dp,
+                    bottom = 16.dp
+                )
+            ) {
+                Text(text = "海面気圧 $seaLevelPressure hPa")
+            }
+
+            Row(
+                modifier = modifier.padding(8.dp)
+            ) {
+                ClickableCard(
+                    text = "$temperatureText ℃",
+                    onClick = null,
+                )
+            }
+
+            EditTextFieldRow(
+                text = defaultAltitudeText,
+                onClickDone = onClickDone,
+                onClickCancel = onClickCancel
             )
         }
-
-        Row(
-            modifier = modifier.padding(
-                top = 8.dp,
-                bottom = 16.dp
-            )
-        ) {
-            Text(text = "海面気圧 $seaLevelPressure hPa")
-        }
-
-        Row(
-            modifier = modifier.padding(8.dp)
-        ) {
-            ClickableCard(
-                text = "$temperatureText ℃",
-                onClick = null,
-            )
-        }
-
-        EditTextFieldRow(
-            text = defaultAltitudeText,
-            onClickDone = onClickDone,
-            onClickCancel = onClickCancel
-        )
     }
 }
 
 @Preview(
     showBackground = true,
+    name = "Light Mode",
+    widthDp = 360
+)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode",
     widthDp = 360
 )
 @Composable
