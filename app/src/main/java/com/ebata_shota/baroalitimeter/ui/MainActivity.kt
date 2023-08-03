ab93d8be1cf90b82a75f9367d3660ad30d2052a5
ebata-shota <maamaaomosiroi@gmail.com>
2023-08-03 23:12:53 +0900

    MainActivityの余計なimportを削除

--- app/src/main/java/com/ebata_shota/baroalitimeter/ui/MainActivity.kt
+++ app/src/main/java/com/ebata_shota/baroalitimeter/ui/MainActivity.kt
@@ -6,11 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import com.ebata_shota.baroalitimeter.domain.extensions.collect
