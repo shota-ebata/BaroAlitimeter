277aef3377212a65555ae6819fbc4718546324b6
ebata-shota <maamaaomosiroi@gmail.com>
2023-08-15 19:05:12 +0900

    OSSライセンス表記

--- app/src/main/java/com/ebata_shota/baroalitimeter/ui/parts/MainTopAppBar.kt
+++ app/src/main/java/com/ebata_shota/baroalitimeter/ui/parts/MainTopAppBar.kt
@@ -1,9 +1,12 @@
 package com.ebata_shota.baroalitimeter.ui.parts
 
+import android.content.Intent
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Divider
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -15,9 +18,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
 import com.ebata_shota.baroalitimeter.R
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
@@ -45,7 +51,7 @@ fun MainTopAppBar(
                         contentDescription = null
                     )
                     DropdownMenu(
-                        modifier = modifier,
+                        modifier = modifier.padding(horizontal = 4.dp),
                         expanded = expanded,
                         onDismissRequest = hideTopAppBarDropdownMenu
                     ) {
@@ -64,7 +70,17 @@ fun MainTopAppBar(
                                 onClickTheme()
                             }
                         )
-//            Divider()
+                        Divider()
+                        val context = LocalContext.current
+                        DropdownMenuItem(
+                            text = {
+                                Text(text = stringResource(id = R.string.oss))
+                            },
+                            onClick = {
+                                hideTopAppBarDropdownMenu()
+                                context.startActivity(Intent(context, OssLicensesMenuActivity::class.java))
+                            }
+                        )
                     }
                 }
             }
