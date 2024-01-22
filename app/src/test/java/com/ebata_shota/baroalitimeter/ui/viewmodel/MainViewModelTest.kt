e3df714900c9e565aa0da6a6d9f3489b75e0b4ab
ebata-shota <maamaaomosiroi@gmail.com>
2024-01-22 23:34:18 +0900

    SensorRepositoryをspyに変換

--- app/src/test/java/com/ebata_shota/baroalitimeter/ui/viewmodel/MainViewModelTest.kt
+++ app/src/test/java/com/ebata_shota/baroalitimeter/ui/viewmodel/MainViewModelTest.kt
@@ -7,26 +7,29 @@ import com.ebata_shota.baroalitimeter.domain.model.Temperature
 import com.ebata_shota.baroalitimeter.domain.model.content.ThemeMode
 import com.ebata_shota.baroalitimeter.domain.repository.CalcRepository
 import com.ebata_shota.baroalitimeter.domain.repository.PrefRepository
-import com.ebata_shota.baroalitimeter.domain.repository.SensorRepository
+import com.ebata_shota.baroalitimeter.infra.repository.CalcRepositoryImpl
+import com.ebata_shota.baroalitimeter.infra.repository.SpySensorRepository
 import com.ebata_shota.baroalitimeter.viewmodel.MainViewModel
 import com.google.firebase.analytics.FirebaseAnalytics
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.spy
 import org.mockito.kotlin.whenever
 
 
 class MainViewModelTest {
 
-    private val sensorRepository: SensorRepository = mock()
+    private val sensorRepository: SpySensorRepository = spy(SpySensorRepository())
     private val prefRepository: PrefRepository = mock()
-    private val calcRepository: CalcRepository = mock()
+    private lateinit var calcRepository: CalcRepository
     private val firebaseAnalytics: FirebaseAnalytics = mock()
 
 
@@ -35,22 +38,23 @@ class MainViewModelTest {
     @OptIn(ExperimentalCoroutinesApi::class)
     @Before
     fun setup() {
-        val dispatcher = UnconfinedTestDispatcher()
+        // memo: TestDispatcherについて https://developer.android.com/kotlin/coroutines/test?hl=ja#standardtestdispatcher
+        val dispatcher = StandardTestDispatcher()
         Dispatchers.setMain(dispatcher)
 
+        calcRepository = CalcRepositoryImpl(dispatcher = dispatcher)
 
-        whenever(sensorRepository.pressureSensorState).thenReturn(
-            MutableStateFlow(
+        runTest {
+            sensorRepository.emitPressureSensorState(
                 Pressure.Success(
                     value = SensorManager.PRESSURE_STANDARD_ATMOSPHERE
                 )
             )
-        )
-        whenever(sensorRepository.temperatureSensorState).thenReturn(
-            MutableStateFlow(
+            sensorRepository.emitTemperatureSensorState(
                 Temperature.HasNotSensor
             )
-        )
+        }
+
         whenever(prefRepository.preferencesFlow).thenReturn(
             MutableStateFlow(
                 PreferencesModel(
