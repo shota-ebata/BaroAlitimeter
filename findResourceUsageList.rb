29d3c9071ebfd7141db909355616ceba6b06aee7
ebata-shota <maamaaomosiroi@gmail.com>
2024-04-01 13:54:49 +0900

    すべてを外部に

--- findResourceUsageList.rb
+++ findResourceUsageList.rb
@@ -29,3 +29,39 @@ def find_file_names_include(search_text)
    end
    return files_with_text
 end
+
+# strings.xmlのパス
+STRINGS_XML_PATH = "app/src/main/res/values/strings.xml"
+
+# Pull Request内のファイル変更を取得
+changed_files = git.modified_files + git.added_files
+
+file_name = STRINGS_XML_PATH
+
+# strings.xmlが変更されたかチェックし、コメントを追加
+if changed_files.include?(file_name)
+    # 変更行の一覧を取得
+    diff = git.diff_for_file(file_name)
+    # 変更行がある場合にのみコメントを出力
+    if diff
+        diff.patch.lines.each do |line|
+            # 差分から追加行を検索
+            if line.match(/^\+{1}[ ].+/)
+                line_text = line.sub("+ ", "")
+                # Stringリソース名取得
+                res_text = line_text.match(/<.+ name=".+">.+<\/.+>/)[0]
+                string_res_name = res_text.sub(/<.+ name="/, "").sub(/">.+<\/.+>/, "")
+                res_use_file_name_list = find_file_names_include("R.string.#{string_res_name}") + find_file_names_include("@string/#{string_res_name}")
+                message_text_list = []
+                message_text_list << "リソース使用箇所\n"
+                message_text_list << res_use_file_name_list.unshift("- ").push("\n")
+                line_number = -1
+                # リソース名を利用している場所を検索する
+                File.open(file_name, "r") do |file|
+                    line_number = get_line_number(file, line_text)
+                end
+                message(message_text_list.join, file: file_name, line: line_number)
+            end
+        end
+    end
+end
