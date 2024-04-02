4a4657ce223a583d34dd620802d7a5a00b0c88f8
ebata-shota <maamaaomosiroi@gmail.com>
2024-04-02 21:12:46 +0900

    修正

--- script/findResourceUsageList.rb
+++ script/findResourceUsageList.rb
@@ -13,13 +13,14 @@ end
 
 def find_file_names_include(search_text)
    # 検索するディレクトリを指定
-   search_dir = "app/src"
+   search_dir = ""
 
    # 特定のテキストを含むファイルの名前を格納する配列を初期化
    files_with_text = []
 
    # 指定したディレクトリ内のファイルを走査して特定のテキストを含むファイルを検索
-   Dir.glob("#{search_dir}/**/*").each do |file_name|
+   Dir.glob("**/*").each do |file_name|
+     next unless file_name.match(/.+\/build\//)
      next unless File.file?(file_name)
 
      # ファイルを開いてテキストを検索
