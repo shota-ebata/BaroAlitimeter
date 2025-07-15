cd71d13351ec2fff51534cd10f93b03c25aad94f
shota-ebata <maamaaomosiroi@gmail.com>
2025-07-15 22:15:27 +0900

    AGP/Gradleのバージョンアップ

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,8 @@ buildscript {
 
 plugins {
     // TargetSDKと最低Version https://developer.android.com/build/releases/gradle-plugin?hl=ja
-    id("com.android.application") version "8.1.1" apply false
-    id("com.android.library") version "8.1.1" apply false
+    id("com.android.application") version "8.8.0" apply false
+    id("com.android.library") version "8.8.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.google.dagger.hilt.android") version "2.44" apply false
     id("com.google.gms.google-services") version "4.3.15" apply false
