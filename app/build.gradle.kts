566b510ad96382bb827067499a00a66136ce5601
shota-ebata <maamaaomosiroi@gmail.com>
2025-07-15 22:54:25 +0900

    versionCodeアップデート

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.ebata_shota.baroalitimeter"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.2"
+        versionCode = 5
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
