2a97552d4d1baad82a86409830b136c2178db76e
shota-ebata <maamaaomosiroi@gmail.com>
2026-02-27 11:45:01 +0900

    versionCodeとversionNameのアップデート

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.ebata_shota.baroalitimeter"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.0.3"
+        versionCode = 6
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
