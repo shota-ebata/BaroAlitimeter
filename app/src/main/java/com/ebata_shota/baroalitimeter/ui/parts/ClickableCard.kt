1b8f2dc848eca079e4a2d1d7fca1ddd1308d2ca5
shota-ebata <maamaaomosiroi@gmail.com>
2026-02-27 11:36:31 +0900

    ClickableCardのcontainerColorの指定を削除

--- app/src/main/java/com/ebata_shota/baroalitimeter/ui/parts/ClickableCard.kt
+++ app/src/main/java/com/ebata_shota/baroalitimeter/ui/parts/ClickableCard.kt
@@ -5,10 +5,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -30,9 +28,6 @@ fun ClickableCard(
             .width(250.dp),
         onClick = onClick ?: {},
         enabled = onClick != null,
-        colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.surface
-        )
     ) {
         Box(
             modifier = modifier
